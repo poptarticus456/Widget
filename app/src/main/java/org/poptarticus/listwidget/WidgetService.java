b9a781423e021da84157086717f32941388d115a
Aaron <Aaron.joseph@outlook.com>
2018-12-29 12:48:29 -0700

    Building the Widget for later Use

---
+++ app/src/main/java/org/poptarticus/listwidget/WidgetService.java
@@ -0,0 +1,12 @@
+package org.poptarticus.listwidget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class WidgetService extends RemoteViewsService {
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new WidgetAdapter(this);
+    }
+}
